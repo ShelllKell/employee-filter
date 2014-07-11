a10e43c4e4b4b53b2e7b59d530f375d67d5d9fa3
Shelby Kelly <shelby.kelly@colorado.edu>
2014-07-11 11:30:17 -0600

    Finished warmup

--- lib/employee_filter.rb
+++ lib/employee_filter.rb
@@ -4,45 +4,41 @@ class EmployeeFilter
     @employees = employees
   end
 
-  def display_time
-
-    "#{date.month}/#{date.day}/#{date.year}"
-  end
 
   def started_before_2006
-    # @employees.each { |date| date[:start_date][0] }.to_s
 
-    # [beaulah]
-
-
-
-
-    # beaulah = []
-    # beaulah = @employees[0]
+    @employees.select { |employee_hash| employee_hash[:start_date].year < 2006 }
 
 
 
   end
 
   def all_with_start_date
-    # ["Beaulah Thompson (Lead Security Facilitator) - 2/9/2005", "Benny Walker (National Research Officer) - 6/23/2006"]
 
-  info = []
-  @employees.each do |employee_hash|
-    first = employee_hash[:first_name]
-    last = employee_hash[:last_name]
-    title = employee_hash[:title]
-    start_date = employee_hash[:start_date].display_time
-    info << "#{first} #{last} #{title} - #{start_date}"
-  end
-  info
+    @employees.map do |employee_hash|
+      "#{employee_hash[:first_name]} #{employee_hash[:last_name]} (#{employee_hash[:title]}) - #{format_time(employee_hash[:start_date])}"
+    end
   end
 
+  private
+
+  def format_time(date_object)
+
+    "#{date_object.month}/#{date_object.day}/#{date_object.year}"
+
+  end
 
 
 
-    # employees[0][:first_name][:last_name]
-
-
+  #
+  # info = []
+  # @employees.each do |employee_hash|
+  #   first = employee_hash[:first_name]
+  #   last = employee_hash[:last_name]
+  #   title = employee_hash[:title]
+  #   start_date = employee_hash[:start_date].display_time
+  #   info << "#{first} #{last} #{title} - #{start_date}"
+  # end
+  # info
 
 end
