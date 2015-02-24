fff1c8cef2256756fc1e1bfc5d1b7e5608d5fab3
Shelby Kelly <shelby.kelly@colorado.edu>
2015-02-24 14:32:35 -0700

    finished formatted date method, specs passing

--- lib/employee_filter.rb
+++ lib/employee_filter.rb
@@ -1,32 +1,22 @@
 class EmployeeFilter
 
-  def initialize(employees)
-    @employees = employees
+  def initialize(employee)
+    @employee = employee
   end
 
 
   def started_before_2006
-
-    @employees.select { |employee_hash| employee_hash[:start_date].year < 2006 }
-
-
-
+    @employee.select { |employee| employee[:start_date].year < 2006}
   end
 
   def all_with_start_date
-
-    @employees.map do |employee_hash|
-      "#{employee_hash[:first_name]} #{employee_hash[:last_name]} (#{employee_hash[:title]}) - #{format_time(employee_hash[:start_date])}"
+    @employee.map do |employee|
+      "#{employee[:first_name]} #{employee[:last_name]} (#{employee[:title]}) - #{format_time(employee[:start_date])}"
     end
   end
 
-  private
-
   def format_time(date_object)
-
     "#{date_object.month}/#{date_object.day}/#{date_object.year}"
-
   end
-
 
 end
